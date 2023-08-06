8fe43919ca46cfcc926c40aeb890798fd7abafd9
guilhermeabell <gpeuvis@gmail.com>
2023-08-05 22:49:39 -0300

    chore: adding eslint and editorconfig to enforce code style

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(3000, () => console.log('Server running on port 3000'));
