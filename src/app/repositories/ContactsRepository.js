42cf60cab637de748c55e3f284f366d5dd68f28e
guilhermeabell <gpeuvis@gmail.com>
2023-08-06 14:29:42 -0300

    chore: replacing uuiv4 with uuid

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,12 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
 const contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Guilherme',
     email: 'guilherme@gmail.com',
     phone: '11988229419',
-    category_id: uuid(),
+    category_id: v4(),
   },
 ];
 
