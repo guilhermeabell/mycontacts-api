34d73aa614c9230edc209e8b9435cc5fcc873e76
guilhermeabell <gpeuvis@gmail.com>
2023-08-12 19:56:14 -0300

    refactor: remove find resource

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -89,13 +89,6 @@ class ContactController {
     // Deletar um registro
     const { id } = req.params;
 
-    const contact = await ContactsRepository.findById(id);
-
-    if (!contact) {
-      // 404: Not found
-      res.status(404).json({ error: 'User not found' });
-    }
-
     await ContactsRepository.delete(id);
     // 204: No Content
     res.sendStatus(204);
