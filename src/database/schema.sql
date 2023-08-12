16be1ed1efa759125e56318eeb21b118e70c1148
guilhermeabell <gpeuvis@gmail.com>
2023-08-12 18:49:33 -0300

    feat: create contacts database table

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS categories (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS contacts (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR UNIQUE,
+  phone VARCHAR,
+  category_id UUID,
+  FOREIGN KEY(category_id) REFERENCES categories(id)
+);
